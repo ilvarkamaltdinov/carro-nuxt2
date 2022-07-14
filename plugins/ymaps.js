10eae27c572d5754c994bf8063fd42d391294f4a
ilvar <ilvar@u2c.su>
2022-07-14 16:09:47 +0300

    правки

--- plugins/ymaps.js
+++ plugins/ymaps.js
@@ -1,2 +1,10 @@
+import Vue from 'vue'
 import YmapPlugin from 'vue-yandex-maps'
+
+const settings = { // you may define your apiKey, lang and version or skip this.
+    apiKey: 'xxx',
+    lang: 'ru_RU',
+    version: '2.1'
+}
 Vue.use(YmapPlugin, settings)
+
