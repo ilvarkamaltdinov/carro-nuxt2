28a19f7accad32fc89393ac71915286482058dca
d-sgnr <43551250+d-sgnr@users.noreply.github.com>
2022-07-25 13:41:10 +0300

    Переделывает хар-ки в мини-карточке и добавляет картинку для избранного

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// const ImageminWebpWebpackPlugin= require("imagemin-webp-webpack-plugin");
+
+// module.exports = {
+//     plugins: [new ImageminWebpWebpackPlugin(
+//       {
+//         config: [{
+//           test: /\.(jpe?g|png)/,
+//           options: {
+//             quality:  75
+//           }
+//         }],
+//         overrideExtension: true,
+//         detailedLogs: false,
+//         silent: false,
+//         strict: true
+//       }
+//     )]
+// };
