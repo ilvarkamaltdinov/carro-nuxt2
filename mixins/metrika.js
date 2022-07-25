8de8ffa5527942ceb58f42f478d0ccd5f48700a1
ilvar <ilvar@u2c.su>
2022-07-25 20:59:40 +0300

    правки

--- mixins/metrika.js
+++ mixins/metrika.js
@@ -9,13 +9,27 @@ export default {
     },
     watch: {
         '$route'() {
-           ym(this.settings.ym, 'hit', `${this.domain}${this.$route.fullPath}`);
+            ym(this.settings.ym, 'hit', `${this.domain}${this.$route.fullPath}`);
         },
     },
     mounted() {
-        console.log('layout loading')
+        let noscript = document.createElement("noscript");
+        noscript.innerHTML = `<div>
+    <img src="https://top-fwz1.mail.ru/counter?id=${settings.mytarget};js=na" style="border:0;position:absolute;left:-9999px;" alt="Top.Mail.Ru" />
+  </div>`
         let ym_script = document.createElement("script");
         ym_script.innerHTML = `
+               
+      var _tmr = window._tmr || (window._tmr = []);
+      _tmr.push({id: "${settings.mytarget}", type: "pageView", start: (new Date()).getTime(), pid: "USER_ID"});
+      (function (d, w, id) {
+        if (d.getElementById(id)) return;
+        var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+        ts.src = "https://top-fwz1.mail.ru/js/code.js";
+        var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+        if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+      })(document, window, "topmailru-code");
+        
         window.dataLayer = window.dataLayer || [];
         (function(m, e, t, r, i, k, a) {
                 m[i] = m[i] || function() {
@@ -34,7 +48,8 @@ export default {
                 ecommerce: "dataLayer"
             });
         `;
-        document.body.appendChild(ym_script);
+        document.head.appendChild(ym_script);
+        document.head.appendChild(noscript);
 
     }
 }
