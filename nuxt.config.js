38b5ecce966dcca96f276d4673244399c594ac90
d-sgnr <43551250+d-sgnr@users.noreply.github.com>
2022-07-26 12:54:54 +0300

    Добавляет в конфиг apple-mobile-web-app

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,9 @@ export default {
             {name: 'http-equiv="X-UA-Compatible', content: 'ie=edge'},
             {name: 'viewport', content: 'width=device-width, initial-scale=1'},
             {hid: 'description', name: 'description', content: ''},
-            {name: 'format-detection', content: 'telephone=no'}
+            {name: 'format-detection', content: 'telephone=no'},
+            {name: 'apple-mobile-web-app-capable',content: 'yes'},
+            {name: 'apple-mobile-web-app-status-bar-style',content: 'black-translucent'}
         ],
         link: [
             {rel: 'icon', href: '/favicon.ico', sizes: 'any'},
