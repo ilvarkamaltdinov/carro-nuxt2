dfd8e282db0e699467a3c04622c34985ea7d3f1b
ilvar <ilvar@u2c.su>
2022-07-27 17:23:13 +0300

    правит редиректы

---
+++ configModules/index.js
@@ -0,0 +1,222 @@
+export const redirects = [
+    {
+        from: '^/taxi',
+        to: 'https://taxi.carro.ru',
+        statusCode: 301
+    },
+    {
+        from: '^/used/dlya-molodezhi',
+        to: '/used/for-youth',
+        statusCode: 301
+    },
+    {
+        from: '^/used/dlya-nee',
+        to: '/used/for-woman',
+        statusCode: 301
+    },
+    {
+        from: '^/used/dlya-semi',
+        to: '/used/for-family',
+        statusCode: 301
+    },
+    {
+        from: '^/used/dlya-dachi',
+        to: '/used/for-giving',
+        statusCode: 301
+    },
+    {
+        from: '^/used/biznes-klass',
+        to: ' /used/business-class',
+        statusCode: 301
+    },
+    {
+        from: '^/used/rabochaya-loshadka',
+        to: '/used/workhorse',
+        statusCode: 301
+    },
+    {
+        from: '^/used/pervyj-avtomobil',
+        to: '/used/first-car',
+        statusCode: 301
+    },
+    {
+        from: '^/used/dlya-taksi',
+        to: '/used/for-taxi',
+        statusCode: 301
+    },
+    {
+        from: '^/used/amerikanskie',
+        to: '/used/american',
+        statusCode: 301
+    },
+    {
+        from: '^/used/kitajskie',
+        to: '/used/chinese',
+        statusCode: 301
+    },
+    {
+        from: '^/used/nemetskie',
+        to: '/used/german',
+        statusCode: 301
+    },
+    {
+        from: 'used/yaponskie',
+        to: '/used/japanese',
+        statusCode: 301
+    },
+    {
+        from: '^/used/otechestvennye',
+        to: ' /used/russian',
+        statusCode: 301
+    },
+    {
+        from: '^/used/frantsuzskie',
+        to: '/used/french',
+        statusCode: 301
+    },
+    {
+        from: '^/used/korejskie',
+        to: '/used/korean',
+        statusCode: 301
+    },
+    {
+        from: '^/banks-rating',
+        to: '/banks-partners',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/coupe',
+        to: '/$1/$2/kupe',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/crossover-5',
+        to: '/$1/$2/vnedoroznik-5-dv',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/hatchback-5 ',
+        to: '/$1/$2/xetcbek-5-dv',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/hatchback-5 ',
+        to: '/$1/$2/xetcbek-5-dv',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/universal-5',
+        to: '/$1/$2/universal-5-dv',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/compact-wen',
+        to: '/$1/$2/kompaktven',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/liftback',
+        to: '/$1/$2/liftbek',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/hatchback-3',
+        to: '/$1/$2/xetcbek-3-dv',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/mini-wen',
+        to: '/$1/$2/miniven',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/all-metal-van',
+        to: '/$1/$2/celnometalliceskii-furgon',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/isothermal',
+        to: '/$1/$2/izotermiceskii-kuzov',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/minibus',
+        to: '/$1/$2/mikroavtobus',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/crossover-3',
+        to: '/$1/$2/vnedoroznik-3-dv',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/coupe-hardtop',
+        to: '/$1/$2/kupe-xardtop',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/van',
+        to: '/$1/$2/furgon',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/pick-up',
+        to: '/$1/$2/pikap',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/onboard-crane',
+        to: '/$1/$2/bortovoi-s-kmu',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/chassis',
+        to: '/$1/$2/sassi',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/flatbed-truck',
+        to: '/$1/$2/bortovoi-gruzovik',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/refrigerator',
+        to: '/$1/$2/refrizerator',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/tourist',
+        to: '/$1/$2/turisticeskii',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/manufactured-van',
+        to: '/$1/$2/promtovarnyi-avtofurgon',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/pickup',
+        to: '/$1/$2/pikap',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/tilt',
+        to: '/$1/$2/tent',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/cabriolet',
+        to: '/$1/$2/kabriolet',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/hatchback-4',
+        to: '/$1/$2/xetcbek-4-dv',
+        statusCode: 301
+    },
+    {
+        from: '^/(used|commercial)/(.*)/benzin',
+        to: '/$1/$2/gasoline',
+        statusCode: 301
+    }
+]
