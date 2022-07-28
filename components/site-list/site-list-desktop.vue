434e192362e5cae12f186d8d1880e78d3807c6f3
ilvar <ilvar@u2c.su>
2022-07-28 12:25:19 +0300

    добавляет Тайтлы в меню в шапке

--- components/site-list/site-list-desktop.vue
+++ components/site-list/site-list-desktop.vue
@@ -1,43 +1,12 @@
 <template>
 	<ul class="main-nav__list site-list site-list--desktop">
-		<li class="site-list__item">
+		<li class="site-list__item" v-for="item in nav" :key="item.link">
 			<nuxt-link
 					@click.native="closeModals"
-					to="/used"
+					:to="item.link"
+					:title="item.title"
 					class="site-list__link">
-				В наличии
-			</nuxt-link>
-		</li>
-		<li class="site-list__item">
-			<nuxt-link
-					@click.native="closeModals"
-					to="/credit"
-					class="site-list__link">
-				Автокредит
-			</nuxt-link>
-		</li>
-		<li class="site-list__item">
-			<nuxt-link
-					@click.native="closeModals"
-					to="/exchange"
-					class="site-list__link">
-				Trade-In
-			</nuxt-link>
-		</li>
-		<li class="site-list__item">
-			<nuxt-link
-					@click.native="closeModals"
-					to="/buyout"
-					class="site-list__link">
-				Выкуп
-			</nuxt-link>
-		</li>
-		<li class="site-list__item">
-			<nuxt-link
-					@click.native="closeModals"
-					to="/about"
-					class="site-list__link">
-				О портале
+				{{item.title }}
 			</nuxt-link>
 		</li>
 	</ul>
@@ -46,13 +15,39 @@
 import {mapMutations} from "vuex";
 
 export default {
+	data() {
+		return {
+			nav: [
+				{
+					title: 'В наличии',
+					link: '/used'
+				},
+				{
+					title:'Автокредит',
+					link:'/credit'
+				},
+				{
+					title:'Trade-In',
+					link:'/exchange'
+				},
+				{
+					title:'Выкуп',
+					link:'/buyout'
+				},
+				{
+					title:'О портале',
+					link:'/about'
+				}
+			]
+		}
+	},
 	methods: {
 		...mapMutations({
 			setModalMenu: 'modal/modal-menu/setModalMenu',
 			setModalMarks: 'modal/modal-marks/setModalMarks',
 			setModalSearch: 'modal/modal-search/setModalSearch',
 		}),
-		closeModals(){
+		closeModals() {
 			this.setModalMenu(false)
 			this.setModalMarks(false)
 			this.setModalSearch(false)
