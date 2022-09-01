47b4c36e3a561c9cce8b22f4bb3c4e7e47dc92dc
ilvar <ilvar@u2c.su>
2022-09-01 13:05:48 +0300

    добавляет запрос на слайды

---
+++ components/picture/picture-slide.vue
@@ -0,0 +1,28 @@
+<template>
+	<picture>
+		<source type="image/webp"
+		        :data-srcset="`${smallWebp} 1x, ${bigWebp} 2x`" />
+		<source media="(min-width: 768px)"
+		        :data-srcset="`${small} 1x, ${big} 2x`" />
+		<img :class="classes"
+		     :data-src="big"
+		     alt=""
+		     loading="lazy"
+		     data-aos="fade-right"
+		     data-aos-duration=" 750"
+		     data-aos-easing="ease-in-out"
+		     data-aos-mirror="true" />
+	</picture>
+</template>
+
+<script>
+export default {
+	props: {
+		classes: String,
+		small: String,
+		smallWebp: String,
+		big: String,
+		bigWebp: String
+	}
+}
+</script>
