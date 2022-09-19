de43621761d7b226d8ae3056a03c0ca453e6f390
ilvar <ilvar@u2c.su>
2022-09-19 14:46:04 +0300

    правит тайтлы в блоге

--- pages/Blog/Articles/index.vue
+++ pages/Blog/Articles/index.vue
@@ -40,6 +40,53 @@ export default {
 	},
 	validate(ctx) {
 		return ctx.store.getters.showBlog
+	},
+	head() {
+		let title = 'Статьи — CARRO'
+		let description_title = 'Статьи — CARRO'
+		let description_text = ' Портал проверенных автомобилей с пробегом CARRO.RU,  весь спектр услуг, Трейд ИН, выкуп, автокредитование. Выгодные цены, еженедельные скидки и подарки, спешите!'
+		let description = description_title ? (description_title + '.' + description_text) : description_text
+		return {
+			title: title,
+			link: [
+				{
+					rel: 'canonical',
+					href: this.domain + this.$route.path
+				}
+			],
+			meta: [
+				{
+					hid: 'desctiption',
+					name: 'description',
+					content: description
+				},
+				{
+					hid: 'og:type',
+					property: 'og:type',
+					content: 'website',
+				},
+				{
+					hid: 'og:url',
+					property: 'og:url',
+					content: this.domain + this.$route.path,
+				},
+				{
+					hid: 'og:title',
+					property: 'og:title',
+					content: title
+				},
+				{
+					hid: 'og:description',
+					property: 'og:description',
+					content: description
+				},
+				{
+					hid: 'og:image',
+					property: 'og:image',
+					content: this.domain + '/carro.png'
+				},
+			]
+		}
 	}
 }
 </script>
