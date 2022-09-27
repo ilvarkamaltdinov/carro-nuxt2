6e5ae691ee886b5fb5367e5935e89c4dca1b0666
ilvar <ilvar@u2c.su>
2022-09-27 12:49:01 +0300

    оптимизация тест

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,7 +24,7 @@ import utm from "@/mixins/utm";
 import metrika from "@/mixins/metrika";
 
 export default {
-	mixins: [utm, metrika],
+	mixins: [utm],
 	watch: {
 		'$route'() {
 			if (this.urlValidate) {
