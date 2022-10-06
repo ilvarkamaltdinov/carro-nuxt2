800c9423fac7a07ecc7019f67fb00747028c6bd7
d-sgnr <43551250+d-sgnr@users.noreply.github.com>
2022-10-06 17:44:09 +0300

    Фиксит опечатки

--- components/application/application-buyout.vue
+++ components/application/application-buyout.vue
@@ -3,7 +3,7 @@
 		<div class="heading-group heading-group--h1">
 			<div class="heading-group__wrap">
 				<h1 class="heading heading--h1">{{ pageTitle }}</h1>
-				<span class="heading-group__label">Выкупи ваш автомобиль за 15 минут</span>
+				<span class="heading-group__label">Выкупим ваш автомобиль за 15 минут</span>
 			</div>
 		</div>
 		<div class="grid__col-12 grid grid--application">
