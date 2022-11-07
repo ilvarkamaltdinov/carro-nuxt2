d69bd0c6ae7a8a12cc28badc869038bb99348645
ilvar <ilvar@u2c.su>
2022-11-07 10:27:27 +0300

    правит пагинацию с выбранным поколением

--- layouts/default.vue
+++ layouts/default.vue
@@ -128,7 +128,8 @@ export default {
 						year_from: Number(this.$route.query.year_from),
 						year_to: Number(this.$route.query.year_to),
 						sort: this.$route.query.sort || this.sort,
-						limit: 8
+						limit: 8,
+						page: Number(this.$route.query.page) || 1,
 					}
 					
 					delete assignVariables.__typename;
