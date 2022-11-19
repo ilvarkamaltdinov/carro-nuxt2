ed0134a9fd8c7b48e6510e51530658e48ef61f2b
ilvar <ilvar@u2c.su>
2022-11-19 22:00:59 +0300

    правит урлы thanks-ов (питерские )

---
+++ pages/Tradein-thanks-ralf.vue
@@ -0,0 +1,22 @@
+<template>
+	<main class="page-main">
+		<div class="grid">
+			<crumbs :crumbs="crumbs" />
+		</div>
+		<div class="grid grid--container">
+			<thanks :page-title="pageTitle"/>
+		</div>
+	</main>
+</template>
+<script>
+import {mapGetters} from "vuex";
+import seoTags from "@/mixins/seoTags";
+import jsonld from "@/mixins/jsonld";
+
+export default {
+	mixins: [seoTags, jsonld],
+	mounted() {
+		setTimeout(function () {window.scrollTo(0, -100);}, 1);
+	}
+}
+</script>
