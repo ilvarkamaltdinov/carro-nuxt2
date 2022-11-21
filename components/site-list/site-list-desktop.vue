21419f8fe062dbe1841144d1a8ea71ce717ad98f
d-sgnr <43551250+d-sgnr@users.noreply.github.com>
2022-11-21 17:42:34 +0300

    Фиксит бенефиты, добавляет в хедер Отзывы

--- components/site-list/site-list-desktop.vue
+++ components/site-list/site-list-desktop.vue
@@ -30,10 +30,10 @@ export default {
 					title:'Trade-In',
 					link:'/exchange'
 				},
-				// {
-				// 	title:'Выкуп',
-				// 	link:'/buyout'
-				// },
+				{
+					title:'Отзывы',
+					link:'/reviews'
+				},
 				{
 					title:'О портале',
 					link:'/about'
