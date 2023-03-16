b93653b3b069aaaeb1a1ec69392d799602e90a52
igamov <dev@igamov.ru>
2023-03-16 15:20:12 +0300

    fix 2

--- components/thanks/thanks.vue
+++ components/thanks/thanks.vue
@@ -1,132 +1,131 @@
 <template>
-  <section class="page-main__thanks thanks grid">
-    <div class="heading-group heading-group--h1">
-      <div class="heading-group__wrap">
-        <h1 class="heading heading--h1">{{ pageTitle }}
-          <span class="heading__promo"></span>
-        </h1>
-        <span class="heading-group__label">{{ currentTitle }}</span>
-      </div>
-    </div>
-    <div class="thanks__block grid__col-12 grid text">
-      <div class="grid__col-8 text__wrap text__wrap--figure">
-        <div class="text__figure figure">
-          <picture-component
-            classes="text__figure-img lazyload"
-            :small="`${require(`~/assets/img/figures/figure-1@1x.png`)}`"
-            :small-webp="`${require(`~/assets/img/figures/figure-1@1x.webp`)}`"
-            :big="`${require(`~/assets/img/figures/figure-1@2x.png`)}`"
-            :big-webp="`${require(`~/assets/img/figures/figure-1@2x.webp`)}`" />
-        </div>
-        <div class="text__content"
-             v-html="currentText"></div>
-      </div>
-      <div v-if="userCar"
-           class="thanks__catalog catalog catalog--single grid__col-4">
-        <component :is="$device.isMobile ? 'catalog-item-large-mobile' :'catalog-item-small-desktop'" :is-form="true"
-                   :has-buttons="false"
-                   :offer="userCar" />
-      </div>
-    </div>
-    <div class="thanks__benefits grid__col-12">
-      <benefits :benefits="benefitsCar" />
-    </div>
-  </section>
+	<section class="page-main__thanks thanks grid">
+		<div class="heading-group heading-group--h1">
+			<div class="heading-group__wrap">
+				<h1 class="heading heading--h1">{{ pageTitle }}
+					<span class="heading__promo"></span>
+				</h1>
+				<span class="heading-group__label">{{ currentTitle }}</span>
+			</div>
+		</div>
+		<div class="thanks__block grid__col-12 grid text">
+			<div class="grid__col-8 text__wrap text__wrap--figure">
+				<div class="text__figure figure">
+					<picture-component
+							classes="text__figure-img lazyload"
+							:small="`${require(`~/assets/img/figures/figure-1@1x.png`)}`"
+							:small-webp="`${require(`~/assets/img/figures/figure-1@1x.webp`)}`"
+							:big="`${require(`~/assets/img/figures/figure-1@2x.png`)}`"
+							:big-webp="`${require(`~/assets/img/figures/figure-1@2x.webp`)}`" />
+				</div>
+				<div class="text__content"
+				     v-html="currentText"></div>
+			</div>
+			<div v-if="userCar"
+			     class="thanks__catalog catalog catalog--single grid__col-4">
+				<component :is="$device.isMobile ? 'catalog-item-large-mobile' :'catalog-item-small-desktop'" :is-form="true" :has-buttons="false"
+				                            :offer="userCar" />
+			</div>
+		</div>
+		<div class="thanks__benefits grid__col-12">
+			<benefits :benefits="benefitsCar" />
+		</div>
+	</section>
 </template>
 <script>
-import { mapActions, mapGetters, mapMutations } from "vuex";
+import {mapActions, mapGetters, mapMutations} from "vuex";
 
 export default {
-  props: {
-    pageTitle: String
-  },
-  data() {
-    return {
-      titles: {
-        "credit": "Автокредит предварительно одобрен",
-        "buyout": "Заявка на выкуп автомобиля",
-        "callback": "Обратный звонок",
-        "free-selection": "Заявка на автоподбор",
-        "trade-in": "TradeIn предварительно одобрен",
-        "hire-purchase": "Рассрочка предварительно одобрена",
-        "station": "Заявка на услугу"
-      }
-    };
-  },
-  methods: {
-    ...mapActions({
-      clearChosenOffer: "modal/modal-choose/clearData"
-    }),
-    ...mapMutations({
-      clear: "form/form/CLEAR_DATA"
-    }),
-    sendVkAds() {
-      if (process.client) {
-        _tmr.push({
-          type: "reachGoal",
-          id: this.settings.vk_ads,
-          value: this.userCar.price,
-          goal: "purchase",
-          params: { product_id: this.userCar.external_id }
-        });
-      }
-    },
-    sendYandexCommercial() {
-      if (process.client) {
-        dataLayer.push({
-          "ecommerce": {
-            "purchase": {
-              "actionField": {
-                "id": this.userOrderId,
-                "goal_id": this.settings.goal_id
-              },
-              "products": [
-                {
-                  "id": this.userCar.external_id,
-                  "name": `${this.userCar.mark.title} ${this.userCar.folder.title}, ${this.userCar.year} года, ${this.userCar.gearbox.title} ${this.userCar.price} руб. - ${this.userCar.external_id}`,
-                  "price": this.userCar.price,
-                  "brand": this.userCar.mark.title,
-                  "category": `'С пробегом/${this.userCar.mark.title}/${this.userCar.folder.title}/${this.userCar.name}`,
-                  "quantity": 1
-                }
-              ]
-            }
-          }
-        });
-      }
-    }
-  },
-  async mounted() {
-    if (!this.userOrderId) {
-      await this.$router.push("/");
-    }
-    if (this.userCar) {
-      this.sendYandexCommercial();
-      this.sendVkAds();
-    }
-  },
-  destroyed() {
-    this.clear();
-    this.clearChosenOffer();
-  },
-  computed: {
-    ...mapGetters({
-      settings: "settings/settings/settings",
-      formType: "form/form/formType",
-      userName: "form/form/userName",
-      userCar: "form/form/userCar",
-      benefitsCar: "benefits/benefitsCar",
-      userOrderId: "form/form/userOrderId"
-    }),
-    currentTitle() {
-      return this.titles[this.formType];
-    },
-    currentText() {
-      return this.texts[this.formType];
-    },
-    texts() {
-      return {
-        "credit": `
+	props: {
+		pageTitle: String
+	},
+	data() {
+		return {
+			titles: {
+				'credit': 'Автокредит предварительно одобрен',
+				'buyout': 'Заявка на выкуп автомобиля',
+				'callback': 'Обратный звонок',
+				'free-selection': 'Заявка на автоподбор',
+				'trade-in': 'TradeIn предварительно одобрен',
+				'hire-purchase': 'Рассрочка предварительно одобрена',
+				'station': 'Заявка на услугу',
+			},
+		}
+	},
+	methods: {
+		...mapActions({
+			clearChosenOffer:'modal/modal-choose/clearData'
+		}),
+		...mapMutations({
+			clear: 'form/form/CLEAR_DATA'
+		}),
+		sendMyTarget() {
+			if (process.client) {
+				_tmr.push({
+					type: 'itemView',
+					productid: this.userCar.external_id,
+					pagetype: 'purchase',
+					list: this.settings.vk_ads_id,
+					totalvalue: this.userCar.price,
+				})
+			}
+		},
+		sendYandexCommercial() {
+			if (process.client) {
+				dataLayer.push({
+					"ecommerce": {
+						"purchase": {
+							"actionField": {
+								"id": this.userOrderId,
+								"goal_id": this.settings.goal_id,
+							},
+							"products": [
+								{
+									"id": this.userCar.external_id,
+									"name": `${this.userCar.mark.title} ${this.userCar.folder.title}, ${this.userCar.year} года, ${this.userCar.gearbox.title} ${this.userCar.price} руб. - ${this.userCar.external_id}`,
+									"price": this.userCar.price,
+									"brand": this.userCar.mark.title,
+									"category": `'С пробегом/${this.userCar.mark.title}/${this.userCar.folder.title}/${this.userCar.name}`,
+									"quantity": 1
+								}
+							]
+						}
+					}
+				})
+			}
+		}
+	},
+	async mounted() {
+		if(!this.userOrderId){
+			await this.$router.push('/')
+		}
+		if (this.userCar) {
+			this.sendYandexCommercial()
+			this.sendMyTarget()
+		}
+	},
+	destroyed() {
+		this.clear();
+		this.clearChosenOffer()
+	},
+	computed: {
+		...mapGetters({
+			settings: 'settings/settings/settings',
+			formType: 'form/form/formType',
+			userName: 'form/form/userName',
+			userCar: 'form/form/userCar',
+			benefitsCar: 'benefits/benefitsCar',
+			userOrderId: 'form/form/userOrderId'
+		}),
+		currentTitle() {
+			return this.titles[this.formType]
+		},
+		currentText() {
+			return this.texts[this.formType]
+		},
+		texts() {
+			return {
+				'credit': `
 						<p class="text__p"> ${this.userName}, автомобиль ${this.userCar?.mark.title}
 						${this.userCar?.folder.title}
 						${this.userCar?.generation?.name}
@@ -138,7 +137,7 @@ export default {
 						<p class="text__p"> После подтверждения заявки вас пригласят в автоцентр для прохождения бесплатного
             тест-драйва и оформления сделки. Оплата возможна как за наличные, так и картой!</p>
 						<p class="text__p">И еще. Передайте менеджеру, что пришли по объявлению на CARRO, чтобы получить подарок и бесплатный комплект шин.</p>`,
-        "trade-in": `
+				'trade-in': `
 						<p class="text__p"> ${this.userName}, автомобиль ${this.userCar?.mark.title}
 						${this.userCar?.folder.title}
 						${this.userCar?.generation?.name}
@@ -150,7 +149,7 @@ export default {
 						<p class="text__p"> После подтверждения заявки вас пригласят в автоцентр для прохождения бесплатного
             тест-драйва и оформления сделки. Оплата возможна как за наличные, так и картой!</p>
 						<p class="text__p">И еще. Передайте менеджеру, что пришли по объявлению на CARRO, чтобы получить подарок и бесплатный комплект шин.</p>`,
-        "hire-purchase": `
+				'hire-purchase': `
 						<p class="text__p"> ${this.userName}, автомобиль ${this.userCar?.mark.title}
 						${this.userCar?.folder.title}
 						${this.userCar?.generation?.name}
@@ -162,20 +161,20 @@ export default {
 						<p class="text__p"> После подтверждения заявки вас пригласят в автоцентр для прохождения бесплатного
             тест-драйва и оформления сделки. Оплата возможна как за наличные, так и картой!</p>
 						<p class="text__p">И еще. Передайте менеджеру, что пришли по объявлению на CARRO, чтобы получить подарок и бесплатный комплект шин.</p>`,
-        "callback": `
+				'callback': `
 						<p class="text__p"> ${this.userName}, благодарим за обращение. Ваша заявка - <b>#${this.userOrderId}</b>.</p>
 						<p class="text__p"> Менеджер свяжется с вами в ближайшее время. Спасибо, что выбрали нас!</p>`,
-        "buyout": `
+				'buyout': `
 						<p class="text__p"> ${this.userName}, благодарим за обращение. Ваша заявка - <b>#${this.userOrderId}</b>.</p>
 						<p class="text__p"> Менеджер свяжется с вами в ближайшее время. Спасибо, что выбрали нас!</p>`,
-        "station": `
+				'station': `
 						<p class="text__p"> ${this.userName}, благодарим за обращение. Ваша заявка - <b>#${this.userOrderId}</b>.</p>
 						<p class="text__p"> Менеджер свяжется с вами в ближайшее время. Спасибо, что выбрали нас!</p>`,
-        "free-selection": `
+				'free-selection': `
 						<p class="text__p"> ${this.userName}, благодарим за обращение. Ваша заявка - <b>#${this.userOrderId}</b>.</p>
-						<p class="text__p"> Менеджер свяжется с вами в ближайшее время. Спасибо, что выбрали нас!</p>`
-      };
-    }
-  }
-};
+						<p class="text__p"> Менеджер свяжется с вами в ближайшее время. Спасибо, что выбрали нас!</p>`,
+			}
+		}
+	},
+}
 </script>
