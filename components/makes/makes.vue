3dfc4c45528c65c5d750e194b299fa638dea3b4c
ilvar <ilvar@u2c.su>
2023-05-24 10:57:47 +0300

    правит список марок в шапке

--- components/makes/makes.vue
+++ components/makes/makes.vue
@@ -1,79 +1,119 @@
 <template>
-	<div class="makes">
-		<div class="makes__wrap">
-			<ul class="makes__list makes__list--popular">
-				<makes-item-popular v-for="item in popular"
-				                    @chooseMark="chooseMark(item)"
-				                    :for-chose-modal="forChoseModal"
-				                    :slug="item.slug"
-				                    @click="setModalMarks(false)"
-				                    :title="item.title"
-				                    :key="item.id" />
-			</ul>
-			<ul class="makes__list makes__list--all">
-				<makes-item v-for="(item, index) in allMarks"
-				            :slug="item.slug"
-				            :active="item === currentMark"
-				            :for-chose-modal="forChoseModal"
-				            @click="clickMarks"
-				            @choose="chooseMark(item)"
-				            :key="index"
-				            :item="item" />
-			</ul>
-		</div>
-	</div>
+  <div class="makes">
+    <div class="makes__wrap modal__makes">
+      <ul class="makes__list makes__list--popular">
+        <makes-item-popular v-for="item in popular"
+                            @chooseMark="chooseMark(item)"
+                            :for-chose-modal="forChoseModal"
+                            :slug="item.slug"
+                            @click="setModalMarks(false)"
+                            :title="item.title"
+                            :key="item.id"/>
+      </ul>
+      <ul class="makes__list makes__list--all makes__list--all-modal">
+        <makes-item v-for="(item, index) in sortedMarks"
+                    :slug="item.slug"
+                    :active="item === currentMark"
+                    :for-chose-modal="forChoseModal"
+                    @click="clickMarks"
+                    @choose="chooseMark(item)"
+                    :key="index"
+                    :item="item"/>
+        <li v-if="showAllButton">
+          <button-typical
+            @click="clickAllMarks"
+            :text="allMarks ? 'Меньше марок' :'Больше марок'"
+            :class="{'button--show-active': allMarks }"
+            class="button--show"/>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <script>
 import {mapActions, mapGetters, mapMutations} from 'vuex'
+import sortBy from "lodash/sortBy";
 
 export default {
-	props: {
-		forChoseModal: {
-			type: Boolean,
-			default: false
-		},
-		sort: String
-	},
-	computed: {
-		...mapGetters({
-			allMarks: 'marks/marks/allMarks',
-			alphabetMarks: 'marks/marks/alphabetMarks',
-			popularMarks: 'marks/marks/popularMarks',
-			currentMark: 'modal/modal-choose/currentMark'
-		}),
-		popular() {
-			return this.popularMarks.slice(0, 10)
-		},
-		// sortedMarks() {
-		// 	if (this.sort === 'alphabet') {
-		// 		console.log('alphabet')
-		// 		return this.alphabetMarks
-		// 	} else {
-		// 		console.log('allmarks')
-		// 		return this.allMarks
-		// 	}
-		// 	// return this.$_.sortBy(this.allMarks, [function (mark) {
-		// 	// 	return mark.offers_count;
-		// 	// }])
-		// }
-	},
-	methods: {
-		...mapActions({
-			chooseMark: 'modal/modal-choose/chooseMark'
-		}),
-		...mapMutations({
-			setModalSearch: 'modal/modal-search/setModalSearch',
-			setModalMenu: 'modal/modal-menu/setModalMenu',
-			setModalMarks: 'modal/modal-marks/setModalMarks'
-		}),
-		clickMarks() {
-			this.setModalMarks(false)
-			this.setModalSearch(false)
-		},
-		markClick(item) {
-			this.setModalMarks(false)
-			this.$router.push('/used/' + item.slug)
-		}
-	}
+  data() {
+    return {
+      allMarks: false,
+    }
+  },
+  props: {
+    forChoseModal: {
+      type: Boolean,
+      default: false
+    },
+    sort: String
+  },
+  computed: {
+    ...mapGetters({
+      marks: 'marks/marks/allMarks',
+      alphabetMarks: 'marks/marks/alphabetMarks',
+      popularMarks: 'marks/marks/popularMarks',
+      currentMark: 'modal/modal-choose/currentMark'
+    }),
+    popular() {
+      return this.popularMarks.slice(0, 10)
+    },
+    showAllButton() {
+      if (this.$device.isMobile && this.marks) {
+        return this.marks.length > 10
+      } else if (this.marks) {
+        return this.marks.length > 21
+      }
+    },
+    sortedMarks() {
+      return sortBy(this.marksList, [function (mark) {
+        return mark.title;
+      }])
+    },
+    marksList() {
+      let marks = [...this.marks]
+      let marksShowNumber = 21
+      if (this.$device.isMobile) {
+        marksShowNumber = 10
+        if (this.allMarks) {
+          return marks
+        } else {
+          marks = marks.sort((a, b) => parseFloat(b.offers_count) - parseFloat(a.offers_count))
+          marks = marks.slice(0, marksShowNumber)
+          marks = marks.sort((a, b) => a.slug.toLowerCase().localeCompare(b.slug.toLowerCase()))
+          return marks
+        }
+      } else {
+        if (this.allMarks) {
+          return marks
+        } else {
+          marks = marks.sort((a, b) => parseFloat(b.offers_count) - parseFloat(a.offers_count))
+          marks = marks.slice(0, marksShowNumber)
+          marks = marks.sort((a, b) => a.slug.toLowerCase().localeCompare(b.slug.toLowerCase()))
+          return marks
+        }
+      }
+    }
+  },
+  methods: {
+    ...mapActions({
+      chooseMark: 'modal/modal-choose/chooseMark'
+    }),
+    ...mapMutations({
+      setModalSearch: 'modal/modal-search/setModalSearch',
+      setModalMenu: 'modal/modal-menu/setModalMenu',
+      setModalMarks: 'modal/modal-marks/setModalMarks'
+    }),
+    clickAllMarks() {
+      this.allMarks = !this.allMarks
+    },
+    clickMarks() {
+      this.setModalMarks(false)
+      this.setModalSearch(false)
+    },
+    markClick(item) {
+      this.setModalMarks(false)
+      this.$router.push('/used/' + item.slug)
+    }
+  }
 }
 </script>
