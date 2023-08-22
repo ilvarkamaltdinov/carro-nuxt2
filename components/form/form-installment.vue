b64d5355fd39a6a04aa4deaebffbbb2b597b8a6c
d-sgnr <43551250+d-sgnr@users.noreply.github.com>
2023-08-22 14:13:43 +0300

    Фиксит рассрочку и добавляет кнопку рассрочки в мобайл

--- components/form/form-installment.vue
+++ components/form/form-installment.vue
@@ -5,8 +5,8 @@
         <h2 class="heading heading--h2">
           Рассрочка
           <span class="heading__promo">
-						0%
-					</span>
+            0%
+          </span>
         </h2>
         <!-- <span class="heading-group__label">Получите одобрение за 5 минут</span> -->
       </div>
@@ -14,85 +14,53 @@
       <!--        percent-->
       <!--        :rating="formProgress" />-->
     </div>
-    <form class="form"
-          @submit.prevent="submitForm()">
-      <fieldset v-if="hasChose"
-                class="form__fieldset">
-        <label
-          :class="{'form__field-wrap--car-active' : currentCar, 'form__field-wrap--error':error === 'invalid_car'}"
+    <form class="form" @submit.prevent="submitForm()">
+      <fieldset v-if="hasChose" class="form__fieldset">
+        <label :class="{ 'form__field-wrap--car-active': currentCar, 'form__field-wrap--error': error === 'invalid_car' }"
           class="form__field-wrap form__field-wrap--car ">
-          <button class="form__field"
-                  @click.prevent="choseCar()">
+          <button class="form__field" @click.prevent="choseCar()">
 
             {{ currentCar ? currentCar.name : 'Выбрать автомобиль' }}
             <span v-if="currentCar">
-							{{ currentCar.price | toCurrency }}
-						</span>
+              {{ currentCar.price | toCurrency }}
+            </span>
 
           </button>
-          <svg-icon name="icon-form"
-                    class="form__car-icon"/>
+          <svg-icon name="icon-form" class="form__car-icon" />
         </label>
-        <div class="catalog form__catalog" v-if="$device.isMobile && currentCar">
-          <catalog-item-large-mobile-form :is-form="true"
-                                          :offer="currentCar"/>
-        </div>
+        <!-- <div class="catalog form__catalog" v-if="$device.isMobile && currentCar">
+          <catalog-item-large-mobile-form :is-form="true" :offer="currentCar" />
+        </div> -->
       </fieldset>
-      <div class="catalog form__catalog"
-           v-if="$device.isMobile && (offer || currentCar)">
-        <catalog-item-large-mobile-form :is-form="true"
-                                        :offer="offer||currentCar"/>
+      <div class="catalog form__catalog" v-if="$device.isMobile && (offer || currentCar)">
+        <catalog-item-large-mobile-form :is-form="true" :offer="offer || currentCar" />
       </div>
-      <form-credit-calculator
-        installment
-        @changePeriod="changePeriod"
-        @changePayment="changePayment"
-        :params="creditParams"
-        :offer="offer || currentCar"/>
+      <form-credit-calculator installment @changePeriod="changePeriod" @changePayment="changePayment"
+        :params="creditParams" :offer="offer || currentCar" />
       <fieldset class="form__fieldset">
-        <label class="form__field-wrap"
-               :class="nameClass">
-          <inputs-input placeholder="ФИО"
-                        @input="handlerInput('name')"
-                        v-model="form.name.value"
-                        type="text"/>
+        <label class="form__field-wrap" :class="nameClass">
+          <inputs-input placeholder="ФИО" @input="handlerInput('name')" v-model="form.name.value" type="text" />
         </label>
-        <label class="form__field-wrap"
-               :class="dateClass">
-          <inputs-input placeholder="Дата рождения"
-                        @input="handlerInput('date')"
-                        @dateMaskComplete="form.date.valid = true"
-                        @onincomplete="form.date.valid = null"
-                        v-model="form.date.value"
-                        mask="date"
-                        type="tel"/>
+        <label class="form__field-wrap" :class="dateClass">
+          <inputs-input placeholder="Дата рождения" @input="handlerInput('date')"
+            @dateMaskComplete="form.date.valid = true" @onincomplete="form.date.valid = null" v-model="form.date.value"
+            mask="date" type="tel" />
         </label>
-        <label class="form__field-wrap"
-               :class="phoneClass">
-          <inputs-input placeholder="Телефон"
-                        @input="handlerInput('phone')"
-                        @phoneMaskComplete="form.phone.valid = true"
-                        @onincomplete="form.phone.valid = null"
-                        v-model="form.phone.value"
-                        mask="phone"
-                        type="tel"/>
+        <label class="form__field-wrap" :class="phoneClass">
+          <inputs-input placeholder="Телефон" @input="handlerInput('phone')" @phoneMaskComplete="form.phone.valid = true"
+            @onincomplete="form.phone.valid = null" v-model="form.phone.value" mask="phone" type="tel" />
         </label>
-        <checkbox-form :error="error === 'agreeRf'"
-                       @change="changeCheckbox($event,'agreeRf')"
-                       label="Подтверждаю наличие гражданства РФ"/>
-        <checkbox-form :error="error === 'agree'"
-                       @change="changeCheckbox($event,'agree')"
-                       label="Согласен на"
-                       link="обработку личных данных"/>
+        <checkbox-form :error="error === 'agreeRf'" @change="changeCheckbox($event, 'agreeRf')"
+          label="Подтверждаю наличие гражданства РФ" />
+        <checkbox-form :error="error === 'agree'" @change="changeCheckbox($event, 'agree')" label="Согласен на"
+          link="обработку личных данных" />
       </fieldset>
-      <button-typical :loading="buttonDisabled"
-                      text="Оставить заявку"
-                      button-class="button--credit button--form"/>
+      <button-typical :loading="buttonDisabled" text="Оставить заявку" button-class="button--credit button--form" />
     </form>
   </div>
 </template>
 <script>
-import {mapGetters, mapMutations, mapActions} from 'vuex'
+import { mapGetters, mapMutations, mapActions } from 'vuex'
 import formValidation from "@/mixins/formValidation";
 import filters from "@/mixins/filters";
 
